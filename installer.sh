8e9c2e301e6b1f05d08d9a0a792520cbe1dc1864
Victor Kartashov <victor.kartashov@gmail.com>
2014-05-23 23:35:03 +0400

    rename: odt-to-txt -> odf-to-txt rename: pyoffice.py -> lodiff.py
    Signed-off-by: Victor Kartashov <victor.kartashov@gmail.com>

--- installer.sh
+++ installer.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 sudo cp startlo.sh /usr/bin/startlo.sh
-sudo cp pyoffice.py /usr/bin/pyoffice.py
-sudo cp  odt-to-txt /usr/bin/odt-to-txt
-sudo chmod +x+x+x /usr/bin/pyoffice.py
+sudo cp lodiff.py /usr/bin/lodiff.py
+sudo cp  odf-to-txt /usr/bin/odf-to-txt
+sudo chmod +x+x+x /usr/bin/lodiff.py
 sudo chmod +x+x+x /usr/bin/startlo.sh
-sudo chmod +x+x+x /usr/bin/odt-to-txt
+sudo chmod +x+x+x /usr/bin/odf-to-txt
 cat gitconfig >> ~/.gitconfig
