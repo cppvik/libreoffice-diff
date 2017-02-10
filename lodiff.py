25702881d34b4d20d0d9dc856421e8dd37bd6a18
Reuben Thomas <rrt@sc3d.org>
2017-02-10 16:49:59 +0000

    Pass absolute paths to LibreOffice
    It does not work with relative paths

--- lodiff.py
+++ lodiff.py
@@ -22,8 +22,8 @@ resolver = local.ServiceManager.createInstanceWithContext("com.sun.star.bridge.U
 context = resolver.resolve("uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext")
 desktop = context.ServiceManager.createInstanceWithContext("com.sun.star.frame.Desktop", context)
 
-url1 = "file://" + str(sys.argv[1])
-url2 = "file://" + str(sys.argv[2])
+url1 = "file://" + str(os.path.abspath(sys.argv[1]))
+url2 = "file://" + str(os.path.abspath(sys.argv[2]))
 
 #print ("First argument: %s" % str(sys.argv[1]))
 #print ("Second argument: %s" % str(sys.argv[2]))
